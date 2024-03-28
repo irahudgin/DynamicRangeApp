87374a4d0073011e4d72dbeff7a4d22a02d77bfe
Ira Hudgin <irahudgin@gmail.com>
2024-03-28 14:44:40 -0400

    updated project with latest versions of FFmpeg wasm, fixed some things and added some cosmetic changes

--- public/javas/dyRange.js
+++ public/javas/dyRange.js
@@ -1,83 +1,101 @@
 // credit to:https://jeromewu.github.io/ffmpeg-wasm-a-pure-webassembly-javascript-port-of-ffmpeg/ for FFmpeg wasm
 
+// instances the ffmpeg
+const { fetchFile, toBlobURL } = FFmpegUtil;
+const { FFmpeg } = FFmpegWASM;
+
+let ffmpeg = new FFmpeg();
+
 var wheel = document.getElementById("loading");
 var text = document.getElementById("loadingText");
-
-// instances the ffmpeg
-const { createFFmpeg, fetchFile } = FFmpeg;
-const ffmpeg = createFFmpeg({
-  corePath: "/scripts/core/dist/ffmpeg-core.js",
-  log: true,
-});
-
-// shows ffmpeg progress in console
-ffmpeg.setLogging(true);
 
 // gets file from user and initiates ubuffer array + loading wheel variable
 const myFile = document.getElementById("myFile");
 
 myFile.onchange = async () => {
-  wheel.style.display = "block";
-  text.style.display = "flex";
+    wheel.style.display = "block";
+    text.style.display = "flex";
 
-  var ubuffer = [];
-  const fileData = myFile.files[0];
-  // loading wheel appearance
-  // converts fileData to arrayBuffer, then uses an asynchronous function to turn it into a Uint8Array ubuffer
-  await fileData.arrayBuffer().then((buffer) => {
-    ubuffer = new Uint8Array(buffer, 0, buffer.byteLength);
-  });
+    var ubuffer = [];
 
-  (async () => {
-    try {
-      await ffmpeg.load();
-
-      // writes inputted movie file to memory in a Uint8Array
-      await ffmpeg.FS("writeFile", `${fileData.name}`, ubuffer);
-      await ffmpeg.run(
-        "-i",
-        `${fileData.name}`,
-        "-vcodec",
-        "copy",
-        "-af",
-        "compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0",
-        `out_${fileData.name}`
-      );
-    } catch (err) {
-      alert(
-        "Browser may not support SharedArrayBuffer at this time, try using chrome or firefox"
-      );
-      location.reload();
+    const fileData = myFile.files[0];
+    const fileSize = myFile.size;
+    let estTime;
+    if (fileSize < 500073111) {
+        estTime = "under 5 minutes";
+    } else if (fileSize > 500073111 && fileSize < 1000073111) {
+        estTime = "around 10 minutes";
+    } else if (fileSize > 1000073111) {
+        estTime = "10 - 20 minutes";
     }
 
-    try {
-      // reads file to const output (still in array form), and then converted to blob
-      const output = await ffmpeg.FS("readFile", `out_${fileData.name}`);
-      const outputBlob = new Blob([output]);
+    text.textContent =
+        "Modifying the audio of your movie file. Est time : " + estTime;
 
-      // Create object URL to get movie blob from memory, and make available for download
-      const objUrl = await URL.createObjectURL(outputBlob);
-      wheel.style.display = "none";
-      text.style.display = "none";
+    let vidFile = await fetchFile(myFile.files[0]);
 
-      var a = window.document.createElement("a");
-      a.href = objUrl;
-      a.download = `out_${fileData.name}`;
+    console.log(vidFile);
 
-      // Append anchor to body.
-      document.body.appendChild(a);
-      a.click();
-      setTimeout(() => {
-        location.reload();
-      }, 2000);
+    (async () => {
+        try {
+            const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
+            await ffmpeg.load({
+                coreURL: await toBlobURL(
+                    `${baseURL}/ffmpeg-core.js`,
+                    "text/javascript"
+                ),
+                wasmURL: await toBlobURL(
+                    `${baseURL}/ffmpeg-core.wasm`,
+                    "application/wasm"
+                ),
+            });
 
-      // Remove anchor from body
-      document.body.removeChild(a);
-    } catch (err) {
-      alert(
-        "Problem with outputting file. Make sure file is a movie file! Page will reload upon closing this box"
-      );
-      location.reload();
-    }
-  })();
+            await ffmpeg.writeFile(`${fileData.name}`, vidFile);
+            await ffmpeg.exec([
+                "-i",
+                `${fileData.name}`,
+                "-vcodec",
+                "copy",
+                "-af",
+                "compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0",
+                `soundsqueezd_${fileData.name}`,
+            ]);
+        } catch (err) {
+            alert("Something went wrong, make sure you're using google chrome");
+            console.log(err);
+        }
+
+        try {
+            // reads file to const output (still in array form), and then converted to blob
+            const output = await ffmpeg.readFile(
+                `soundsqueezd_${fileData.name}`
+            );
+            const outputBlob = new Blob([output], { type: "video/mp4" });
+
+            // Create object URL to get movie blob from memory, and make available for download
+            const objUrl = URL.createObjectURL(outputBlob);
+            wheel.style.display = "none";
+            text.textContent = "Done!";
+
+            let uploadButton =
+                document.getElementsByClassName("custom-file-upload")[0];
+            uploadButton.style.display = "none";
+
+            let middleDiv = document.getElementsByClassName("formDiv")[0];
+
+            var a = window.document.createElement("a");
+            a.href = objUrl;
+            a.download = `soundsqueezd_${fileData.name}`;
+            a.className = "getFileButton";
+            a.textContent = "Get File";
+
+            // Append anchor to body.
+            middleDiv.appendChild(a);
+        } catch (err) {
+            alert(
+                "Problem with outputting file. Make sure file is a movie file! Page will reload upon closing this box"
+            );
+            console.log(err);
+        }
+    })();
 };
